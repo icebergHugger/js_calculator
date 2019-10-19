16f0c8f9c10927ee5c6ee7127120a4761ccc3912
icebergHugger <kolbeinn.floki@gmail.com>
2019-10-19 20:25:39 +0000

    please

---
+++ calculator.js
@@ -0,0 +1,45 @@
+let displayValue = "";
+let maxStringLength = 10;
+let operators = ["+","-","/","x"]
+
+function updateDisplay() {
+  document.querySelector("h1").innerHTML = displayValue;
+}
+
+let num = document.querySelectorAll(".number");
+num.forEach(function(number) {
+  number.addEventListener("click", addNumber);
+});
+
+function addNumber() {
+  let number = event.target.innerHTML;
+  if(displayValue.length < maxStringLength){
+    displayValue += number;
+    updateDisplay();
+  }
+}
+
+document.querySelector(".clearButton").onclick = function(){
+  displayValue = "";
+  updateDisplay();
+};
+
+document.querySelector(".delButton").onclick = function(){
+  let delString = displayValue.slice(0, -1);
+  displayValue = delString;
+  updateDisplay();
+};
+
+document.querySelector(".equalButton").addEventListener("click", findOperators);
+
+function findOperators(){
+  for(let i = 0; i < operators.length; i++) {
+    if(displayValue.includes(operators[i])) {
+      calcString(operators[i]);
+    }
+  }
+}
+
+function calcString(operator){
+  
+}
