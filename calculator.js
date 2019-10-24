7037831ca5ce28181eeba56309c7c85f7fd4dc74
icebergHugger <kolbeinn.floki@gmail.com>
2019-10-24 18:05:06 +0000

    hopefully final solution

--- calculator.js
+++ calculator.js
@@ -1,5 +1,7 @@
 let displayValue = "";
 let maxStringLength = 10;
+const firstPriority = ["x", "/"];
+const secondPriority = ["+", "-"]
 let operators = ["+","-","/","x"]
 
 function updateDisplay() {
@@ -30,7 +32,30 @@ document.querySelector(".delButton").onclick = function(){
   updateDisplay();
 };
 
-document.querySelector(".equalButton").addEventListener("click", calc);
+
+document.querySelector(".equalButton").addEventListener("click", function(){
+  equationParting(displayValue.split(""));
+});
+
+function equationParting(array) {
+  //Crate array with index of all operators
+  let operatorArray = [];
+  array.forEach(function(item, index) {
+    if(operators.includes(item)){
+      operatorArray.push(index + item);
+    }
+  });
+
+  operatorArray.forEach(function(item, index){
+    if(firstPriority.includes(item.substr(1))){
+      let part = array.splice(index - 1, array.lenght - index + 1);
+      console.log(part);
+    }
+  });
+
+  console.log(operatorArray);
+}
+
 
 function calc() {
   let operatorCount = 0;
@@ -41,7 +66,12 @@ function calc() {
       if(operator == item){
         if(operatorCount == 1){
           let part = calcArray.slice(0, index);
-          calculatePart(part, firstOperator);
+          calcArray = calcArray.slice(index, calcArray.length);
+          console.log(calcArray);
+          let newNum = calculatePart(part, firstOperator);
+          console.log(newNum);
+          let newCalcArray = newNum.concat(calcArray);
+          console.log(newCalcArray);
         }
         firstOperator = operator;
         operatorCount++;
@@ -51,8 +81,27 @@ function calc() {
 }
 
 function calculatePart(array, operator){
-  console.log(array, operator);
-  let num1 = array.slice(0, array.indexOf(operator));
-  let num2 = array.slice(array.indexOf(operator) + 1, array.length);
-  console.log(num1, num2);
+  let num1 = array.slice(0, array.indexOf(operator)).join("");
+  let num2 = array.slice(array.indexOf(operator) + 1, array.length).join("");
+  let newNum = calcSwitch(num1, num2, operator);
+  return newNum.toString().split("");
+}
+
+function calcSwitch(num1, num2, operator){
+  switch (operator) {
+    case "+":
+      return Number(num1) + Number(num2);
+      break;
+    case "-":
+      return Number(num1) - Number(num2);
+      break;
+    case "/":
+      return Number(num1) / Number(num2);
+      break;
+    case "x":
+      return Number(num1) * Number(num2);
+      break;
+    default:
+      return "error";
+  }
 }
